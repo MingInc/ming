ae1d51a74e8e8f4971ff12c10c6eefe165a4acdd
13x54n <laxman.rai.07.26@gmail.com>
2025-11-14 16:41:32 -0500

    Working on landing page

--- components/ui/focus-cards.tsx
+++ components/ui/focus-cards.tsx
@@ -1,91 +1,47 @@
 "use client";
 
-import React, { useState } from "react";
-import { cn } from "@/lib/utils";
 import Link from "next/link";
 
-export const Card = React.memo(
-  ({
-    card,
-    index,
-    hovered,
-    setHovered,
-  }: {
-    card: any;
-    index: number;
-    hovered: number | null;
-    setHovered: React.Dispatch<React.SetStateAction<number | null>>;
-  }) => (
-    <div
-      onMouseEnter={() => setHovered(index)}
-      onMouseLeave={() => setHovered(null)}
-      className={cn(
-        "rounded-lg relative bg-gray-100 dark:bg-neutral-900 overflow-hidden h-60 md:h-96 w-full transition-all duration-300 ease-out",
-        hovered !== null && hovered !== index && "blur-sm scale-[0.98]"
-      )}
-    >
-      <img
-        src={card.src}
-        alt={card.title}
-        className="object-cover absolute inset-0"
-      />
-      <div
-        className={cn(
-          "absolute inset-0 bg-black/50 flex items-end py-8 px-4 transition-opacity duration-300",
-          hovered === index ? "opacity-100" : "opacity-0"
-        )}
-      >
-        <div className="bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-200">
-          <p className="text-xl md:text-2xl font-medium">
-            {card.title}
-          </p>
-
-          <Link href="/">Learn More</Link>
-        </div>
-      </div>
-    </div>
-  )
-);
-
-Card.displayName = "Card";
-
-type Card = {
-  title: string;
-  src: string;
-};
-
 export function FocusCards() {
-  const [hovered, setHovered] = useState<number | null>(null);
 
   const cards = [
     {
-      title: "Sunya",
-      src: "https://images.unsplash.com/photo-1682637275957-8e62180efd1b?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Sunya - Scan. Detect. Secure.",
+      src: "https://images.unsplash.com/photo-1698423846501-cc5c25d07e85?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      description: "Smart Contracts analysis using AI and large datasets of vulnerabilities, so your business can focus on other things."
     },
     {
-      title: "Lypto",
-      src: "https://images.unsplash.com/photo-1556742031-c6961e8560b0?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Lypto - Unified Wallet & Payments.",
+      src: "https://images.unsplash.com/photo-1726137569807-c0ce211ccbec?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      description: "For personal, a non-custodial wallet. For business, we offer payment processor, POS (Stablecoins as Digital Assets). "
     },
     {
-      title: "iSendMoney",
+      title: "iSendMoney - Borderless, No Hidden Fees.",
       src: "https://images.unsplash.com/photo-1580048915913-4f8f5cb481c4?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      description: "Borderless P2P remittance platform built on top of Blockchain and AI agents on customer support, dispute handling, etc."
     },
     {
-      title: "Rose",
-      src: "https://images.unsplash.com/photo-1685034759882-34583cece8e1?q=80&w=1001&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Rose - Global Device Storage.",
+      src: "https://images.unsplash.com/photo-1480843669328-3f7e37d196ae?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      description: "Store your data over blockchain with privacy. We make sure of secure, accessible storage for your data as strong as NFTs."
     }
   ];
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-10 max-w-5xl mx-auto md:px-8 w-full">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mx-auto w-full">
       {cards.map((card, index) => (
-        <Card
-          key={card.title}
-          card={card}
-          index={index}
-          hovered={hovered}
-          setHovered={setHovered}
-        />
+        <div key={index}>
+          <img
+            key={card.title}
+            className="h-70 object-cover w-full"
+            src={card.src}
+          />
+          <p className="text-lg font-medium mt-1">{card.title}</p>
+          <p>
+            {card.description}
+          </p>
+          <Link href="">Learn More</Link>
+        </div>
       ))}
     </div>
   );
